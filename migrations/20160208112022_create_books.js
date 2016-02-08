16d456cb228b9e60eac44c2303e25ac8e9012341
Kinsey Ashby <kinseyzaire@gmail.com>
2016-02-08 14:10:30 -0700

    fleshed out basics

--- migrations/20160208112022_create_books.js
+++ migrations/20160208112022_create_books.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
     table.increments();
     table.string('title');
     table.string('genre');
-    table.string('description');
+    table.text('description');
     table.string('cover_url');
     table.string('author_id');
   })
